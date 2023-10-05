e656d9b7b6d25ac4ac65f7e266b3cc414458c9b2
Ulrich HOUNGBO <houngbou@gmail.com>
2023-10-05 13:08:03 +0100

    Advanced task

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -1,13 +1,16 @@
 #!/usr/bin/env python3
-""" Duck typing - first element of a sequence"""
+""" 11. More involved type annotations
+"""
 
 from typing import Any, Mapping, TypeVar, Union
 
 T = TypeVar('T')
 
 
-def safely_get_value(dct: Mapping, key: Any, default: Union[T, None] = None) -> Union[Any, T]:
-    """ Returns value of key in dct or default """
+def safely_get_value(dct: Mapping, key: Any,
+                     default: Union[T, None] = None) -> Union[Any, T]:
+    """ Gets value safely.
+    """
     if key in dct:
         return dct[key]
     else:
