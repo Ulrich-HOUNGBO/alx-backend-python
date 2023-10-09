0ddea9eb5eb7618a016fe2d70d40cccf4b781de4
Ulrich HOUNGBO <houngbou@gmail.com>
2023-10-09 21:51:08 +0100

    a function (do not create an async function, use the regular function syntax to do this) task_wait_random that takes an integer max_delay and returns a asyncio.Task

--- 0x01-python_async_function/3-tasks.py
+++ 0x01-python_async_function/3-tasks.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 """Measure the runtime of a coroutine"""
+import asyncio
 
 wait_random = __import__('0-basic_async_syntax').wait_random
-import asyncio
 
 
 def task_wait_random(max_delay: int) -> asyncio.Task:
